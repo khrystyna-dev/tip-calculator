a8963827fd1ab24f67d864efebd7685622693ffc
Chris <tynkastya@gmail.com>
2023-09-07 14:55:26 +0300

    added project

---
+++ app/src/androidTest/java/com/example/tip_calculator/ExampleInstrumentedTest.kt
@@ -0,0 +1,116 @@
+package com.example.tip_calculator
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.hamcrest.Matchers.containsString
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.tip_calculator", appContext.packageName)
+    }
+
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun calculate_20_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50"))
+
+        onView(withId(R.id.calculate_button)).perform(click())
+
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$10.00"))))
+    }
+
+    @Test
+    fun calculate_18_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50"))
+
+        onView(withId(R.id.option_eighteen_percent)).perform(click())
+
+        onView(withId(R.id.calculate_button)).perform(click())
+
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$9.00"))))
+    }
+
+    @Test
+    fun calculate_15_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50"))
+
+        onView(withId(R.id.option_fifteen_percent)).perform(click())
+
+        onView(withId(R.id.calculate_button)).perform(click())
+
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$8.00"))))
+    }
+
+    @Test
+    fun calculate_20_without_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("66"))
+
+        onView(withId(R.id.option_twenty_percent)).perform(click())
+
+        onView(withId(R.id.round_up_switch)).perform(click())
+
+        onView(withId(R.id.calculate_button)).perform(click())
+
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$13.20"))))
+    }
+
+    @Test
+    fun calculate_18_without_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("66"))
+
+        onView(withId(R.id.option_eighteen_percent)).perform(click())
+
+        onView(withId(R.id.round_up_switch)).perform(click())
+
+        onView(withId(R.id.calculate_button)).perform(click())
+
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$11.88"))))
+    }
+
+    @Test
+    fun calculate_15_without_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("66"))
+
+        onView(withId(R.id.option_fifteen_percent)).perform(click())
+
+        onView(withId(R.id.round_up_switch)).perform(click())
+
+        onView(withId(R.id.calculate_button)).perform(click())
+
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$9.90"))))
+    }
+}
